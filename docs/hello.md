71fd1adba0abd5d819133b1c85f512fe56dd5e42
Fight_xing <1123314987@qq.com>
2026-03-14 18:02:33 +0800

    开始部署

---
+++ docs/hello.md
@@ -0,0 +1,5 @@
+# Hello World!
+
+你好世界！
+
+或者……Ciallo world~
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "西格蒙德的个人站点",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "blog.sigmund.moe",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
